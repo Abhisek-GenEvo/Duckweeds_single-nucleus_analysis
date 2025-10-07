3c9bfa0319cc92e1f058779eb8132c05e83fd3c7
Abhisek-GenEvo <chakrabortyabhi2013@gmail.com>
2025-10-07 19:40:15 +0200

    Update Merged_replicates_analysis.R

--- Lemna/Merged_replicates_analysis.R
+++ Lemna/Merged_replicates_analysis.R
@@ -1,42 +1,34 @@
 library(Seurat)
-P1_lemna <- readRDS("Lemna_seurat1.rds")
-P2_lemna <- readRDS("Lemna_seurat2.rds")
-P1_lemna <- subset(P1_lemna, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & nCount_RNA > 500 & nCount_RNA < 10000 & percent.mt < 0.25 & percent.cp < 0.15)
-print(P1_lemna)
-P2_lemna <- subset(P2_lemna, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & nCount_RNA > 500 & nCount_RNA < 10000 & percent.mt < 0.25 & percent.cp < 0.15)
-print(P2_lemna)
-P_merged <- merge(P1_lemna,
-              y = P2_lemna,
+seurat_obj1 <- readRDS("Lemna_seurat1.rds")
+seurat_obj2 <- readRDS("Lemna_seurat2.rds")
+seurat_obj1 <- subset(seurat_obj1, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & nCount_RNA > 500 & nCount_RNA < 10000 & percent.mt < 0.25 & percent.cp < 0.15)
+seurat_obj2 <- subset(seurat_obj2, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & nCount_RNA > 500 & nCount_RNA < 10000 & percent.mt < 0.25 & percent.cp < 0.15)
+obj_merged <- merge(seurat_obj1,
+              y = seurat_obj2,
               add.cell.ids = c("P1", "P2"),
               project = "Merged_P")
-P_merged <- NormalizeData(P_merged, normalization.method = "LogNormalize", scale.factor = 10000)
-P_merged <- FindVariableFeatures(P_merged, selection.method = "vst", nfeatures = 3000)
-all.genes.P_merged <- rownames(P_merged)
-P_merged <- ScaleData(P_merged, features = all.genes.P_merged)
-P_merged <- RunPCA(P_merged, features = VariableFeatures(object = P_merged))
-P_merged <- FindNeighbors(P_merged, dims = 1:30, reduction = "pca")
-P_merged <- FindClusters(P_merged, resolution = 0.5, cluster.name = "unintegrated_clusters")
-P_merged <- RunUMAP(P_merged, reduction = "pca", dims = 1:30, reduction.name = "umap.unintegrated")
-P_merged <- IntegrateLayers(
-  object = P_merged,
+obj_merged <- NormalizeData(obj_merged, normalization.method = "LogNormalize", scale.factor = 10000)
+obj_merged <- FindVariableFeatures(obj_merged, selection.method = "vst", nfeatures = 3000)
+all.genes.obj_merged <- rownames(obj_merged)
+obj_merged <- ScaleData(obj_merged, features = all.genes.obj_merged)
+obj_merged <- RunPCA(obj_merged, features = VariableFeatures(object = obj_merged))
+obj_merged <- FindNeighbors(obj_merged, dims = 1:30, reduction = "pca")
+obj_merged <- FindClusters(obj_merged, resolution = 0.5, cluster.name = "unintegrated_clusters")
+obj_merged <- RunUMAP(obj_merged, reduction = "pca", dims = 1:30, reduction.name = "umap.unintegrated")
+obj_merged <- IntegrateLayers(
+  object = obj_merged,
   method = CCAIntegration,
   orig.reduction = "pca",
   new.reduction = "integrated.cca",
   verbose = FALSE
 )
-P_merged[["RNA"]] <- JoinLayers(P_merged[["RNA"]])
-P_merged <- FindNeighbors(P_merged, reduction = "integrated.cca", dims = 1:30)
-P_merged <- FindClusters(P_merged, resolution = 0.5, cluster.name = "cca_clusters")
-P_merged <- RunUMAP(P_merged, reduction = "integrated.cca", dims = 1:30, reduction.name = "umap.cca")
-pdf(file = '/lustre/project/m2_jgu-evoltroph/achakrab/LA_cellranger_analysis/Final_separate_seurat_analysis/after_QC_merging/P1P2_umap_complete_cca1.pdf', width=10, height=10)
-DimPlot(P_merged, reduction = "umap.cca", group.by = "orig.ident")
-dev.off()
-pdf(file = '/lustre/project/m2_jgu-evoltroph/achakrab/LA_cellranger_analysis/Final_separate_seurat_analysis/after_QC_merging/P1P2_umap_complete_cca2.pdf', width=10, height=10)
+obj_merged[["RNA"]] <- JoinLayers(obj_merged[["RNA"]])
+obj_merged <- FindNeighbors(obj_merged, reduction = "integrated.cca", dims = 1:30)
+obj_merged <- FindClusters(obj_merged, resolution = 0.5, cluster.name = "cca_clusters")
+obj_merged <- RunUMAP(obj_merged, reduction = "integrated.cca", dims = 1:30, reduction.name = "umap.cca")
+DimPlot(obj_merged, reduction = "umap.cca", group.by = "orig.ident")
 DimPlot(P_merged, reduction = "umap.cca", label = TRUE)
-dev.off()
-pdf(file = '/lustre/project/m2_jgu-evoltroph/achakrab/LA_cellranger_analysis/Final_separate_seurat_analysis/after_QC_merging/P1P2_umap_complete_cca3.pdf', width=10, height=10)
 DimPlot(P_merged, reduction = "umap.cca", split.by = "orig.ident", label = TRUE)
-dev.off()
-saveRDS(P_merged, file = "/lustre/project/m2_jgu-evoltroph/achakrab/LA_cellranger_analysis/Final_separate_seurat_analysis/after_QC_merging/Before_markers_P1P2_CCA.rds")
-markers_P1P2 <- FindAllMarkers(P_merged, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-write.csv(markers_P1P2, "/lustre/project/m2_jgu-evoltroph/achakrab/LA_cellranger_analysis/Final_separate_seurat_analysis/after_QC_merging/P1P2_markers_louvian_CCA.csv", row.names = FALSE, quote = FALSE)
+markers <- FindAllMarkers(obj_merged, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+write.csv(markers, "all_markers.csv", row.names = FALSE, quote = FALSE)
+saveRDS(obj_merged, file = "Merged_seurat_object.rds")
