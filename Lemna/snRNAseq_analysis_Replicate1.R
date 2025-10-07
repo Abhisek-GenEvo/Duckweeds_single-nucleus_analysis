8a983914fa3601e8a92bfe9db05b0390b0db8c04
Abhisek-GenEvo <chakrabortyabhi2013@gmail.com>
2025-10-07 19:20:15 +0200

    Create snRNAseq_analysis_Replicate1.R

---
+++ Lemna/snRNAseq_analysis_Replicate1.R
@@ -0,0 +1,17 @@
+library(Seurat)
+Lemna_seurat1 <- Read10X_h5("filtered_feature_bc_matrix_P1.h5")
+seurat_obj1 <- CreateSeuratObject(counts = Lemna_seurat1, project = "seurat_obj1")
+seurat_obj1 <- subset(seurat_obj1, subset = nCount_RNA > 500)
+
+library(scDblFinder)
+library(SingleCellExperiment)
+set.seed(123)
+sce_obj1 <- as.SingleCellExperiment(seurat_obj1)
+sce_obj1 <- scDblFinder(sce_obj1)
+seurat_obj1$scDblFinder.class <- sce_obj1$scDblFinder.class
+seurat_obj1$scDblFinder.score <- sce_obj1$scDblFinder.score
+seurat_obj1 <- subset(seurat_obj1, subset = scDblFinder.class == 'singlet')
+seurat_obj1[["percent.cp"]] <- PercentageFeatureSet(seurat_obj1, pattern = "^CP-")
+seurat_obj1[["percent.mt"]] <- PercentageFeatureSet(seurat_obj1, pattern = "^MT-")
+VlnPlot(seurat_obj1, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.cp"), ncol = 4)
+FeatureScatter(seurat_obj1, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
